486192cb8d07eb8bcbdb65829b8740a753e9e827
anvesh <anveshchamakuri20@gmail.com>
2020-10-20 15:03:29 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,142 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'dynamiclayout';
+  rowvalue = [12];
+  div1;
+  div2;
+  btns;
+  previewbtn;
+  smartphonediv;
+  tabletdiv;
+  laptopdiv;
+  icns;
+  heightval = [];
+  indval;
+  Val;
+  private regForm: any;
+  constructor(private modalService: NgbModal, private formBuilder: FormBuilder) { }
+  aab;
+  ngOnInit() {
+    this.aab = [{rowsize: '4', height: '20px' },
+    {rowsize: '3', height: '30px' }, {rowsize: '3', height: '40px' },
+    {rowsize: '4', height: '50px' }];
+    this.regForm = this.formBuilder.group({
+      heightval: ['', Validators.required]
+    });
+    this.div1 = true;
+    this.div2 = false;
+    this.btns = false;
+    this.previewbtn = true;
+    this.smartphonediv = false;
+    this.tabletdiv = false;
+    this.laptopdiv = false;
+    this.icns = false;
+    this.heightval = [];
+  }
+  onAddRow() {
+    Array.prototype.push.apply(this.rowarray, this.rowvalue);
+   // this.heightval[this.rowarray.length - 1 ] = 200;
+    console.log(this.rowarray.length);
+  }
+  onRemoveRow(rowIndex: number) {
+    this.rowarray.splice(rowIndex, 1);
+  }
+  editrow(rowIndex) {
+    this.indval = rowIndex;
+    this.regForm.value.heightval = '';
+  }
+
+   layout(b) {
+    this.Val = b;
+  }
+  aa;
+  layoutsave() {
+    debugger
+    this.rowarray.splice(this.indval, 1);
+    Array.prototype.push.apply(this.rowarray, this.Val);
+    console.log(this.rowarray);
+    this.aa = [this.regForm.value.heightval] ;
+    Array.prototype.push.apply(this.heightval, this.aa);
+    //this.heightval[this.indval] = this.regForm.value.heightval;
+  }
+  // tslint:disable-next-line: member-ordering
+  closeResult = '';
+
+  /***Edit* */
+  open(content) {
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title', size: 'lg' }).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+  preview() {
+    debugger
+    console.log(this.rowarray);
+    this.div1 = false;
+    this.div2 = true;
+    this.btns = true;
+    this.previewbtn = false;
+    this.icns = true;
+  }
+  previewback() {
+    this.div1 = true;
+    this.div2 = false;
+    this.previewbtn = true;
+    this.btns = false;
+    this.smartphonediv = false;
+    this.tabletdiv = false;
+    this.laptopdiv = false;
+  }
+  smartphone() {
+    this.div2 = false;
+    this.smartphonediv = true;
+    this.tabletdiv = false;
+    this.laptopdiv = false;
+  }
+  tablet() {
+    this.div2 = false;
+    this.smartphonediv = false;
+    this.tabletdiv = true;
+    this.laptopdiv = false;
+  }
+  laptop() {
+    this.div2 = false;
+    this.smartphonediv = false;
+    this.tabletdiv = false;
+    this.laptopdiv = true;
+  }
+  rowarray = ['12'];
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
+  }
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDialogModule} from '@angular/material/dialog';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ ModalModule, BrowserModule, FormsModule, ReactiveFormsModule, BrowserAnimationsModule, 
+    MatButtonModule, MatDialogModule, NgbModule, DragDropModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
